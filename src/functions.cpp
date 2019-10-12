4a0daf8a753d97dbe355d299e0a18b6756cefc72
k-vernooy <kai.a.vernooy@gmail.com>
2019-10-12 09:56:29 -0400

    Finish check for shape death

--- src/functions.cpp
+++ src/functions.cpp
@@ -3,6 +3,7 @@
 #include <ncurses.h>
 #include <cstring>
 #include <random>
+#include <algorithm>
 
 using namespace std;
 
@@ -35,6 +36,10 @@ Screen::Screen(string screenstr) {
         }
         window.push_back(rowVec);
     }
+}
+
+vector<vector<string> > Screen::getScr() {
+    return window;
 }
 
 void Screen::draw() {
@@ -70,7 +75,9 @@ void Screen::gameOver() {
 
 // basically a constructor to generate a new
 // random shape and fill attributes:
-void Shape::generate() {
+void Shape::generate(vector<vector<string> > window) {
+
+    currentWin = window;
 
     random_device rd; 
     mt19937 eng(rd()); 
@@ -106,35 +113,83 @@ string Screen::getChar(int x, int y) {
     return window[x][y];
 }
 
+vector<int> Shape::charCoords(vector<vector<bool> > shape, int pos[2]) {
+    vector<int> coords;
+    int currentPos[2] = { trCoord[0] + defaultPos[1] + 1, trCoord[1] + defaultPos[0]};
+    // top right coord is pos[2]
+    for ( int i = 0; i < shape.size(); i++ ) {
+        for ( int j = 0; j < shape[i].size(); j++ ) {
+            if ( shape[i][j] ) {
+                
+                int thisPos[2] = { currentPos[0] + i, currentPos[1] + ( 2 * j ) - 1 };
+                coords.push_back(thisPos[0]);
+                coords.push_back(thisPos[1]);
+                // add this coord to the array
+            }
+        }
+    }
+    return coords;
+}
+
 void Shape::drop() {
-    // bool cannotDrop;
+    bool cannotDrop = false;
+    vector<int> coords = charCoords(selected, trCoord);
+    for ( int i = 0; i < coords.size(); i = i + 2) {
+        string check = currentWin[coords[i + 1]][coords[i + 1]];
+        if ( check == "█" || check == "─" ) {
+            cannotDrop = true;
+        };
+    }
 
-    // // if any one of the shape's coordinates cannot move down the full amount, die:
-    // for ( int i = 3; i > 0; i-- ) {
-    //     for ( int x = 0; x < 4; x++ ) {
-    //         // going up rows of shape, over cells
-    //         if ( selected[i][x] ) {
-    //             // this block needs to descend by 1; check coord below;
-    //             int position[2] = { trCoord[0] + i - 1, trCoord[1] + x };
-    //         }
-    //     }
-    // }
-    
-    // if ( cannotDrop ) {
-    //     // gameOver();
-    // }
-    // else {
-    //     trCoord[0] += 1;
-    // }
+    if ( cannotDrop ) {
+        // gameOver();
+    }
+    else {
+        trCoord[0]++;
+        isdropping--;
+        mvprintw(0,0,to_string(isdropping).c_str());
+    }
 
-    trCoord[0]++;
 
-    isdropping--;
-    mvprintw(0,0,to_string(isdropping).c_str());
 }
 
 void Shape::rotate() {
     // function to rotate the matrix
+
+    // Consider all squares one by one 
+    for (int x = 0; x < 4 / 2; x++) { 
+        // Consider elements in group of 4 in  
+        // current square 
+        for (int y = x; y < 4 - x - 1; y++) { 
+            // store current cell in temp variable 
+            int temp = selected[x][y]; 
+  
+            // move values from right to top 
+            selected[x][y] = selected[y][4-1-x]; 
+  
+            // move values from bottom to right 
+            selected[y][4-1-x] = selected[4-1-x][4-1-y]; 
+  
+            // move values from left to bottom 
+            selected[4-1-x][4-1-y] = selected[4-1-y][x]; 
+  
+            // assign temp to left 
+            selected[4-1-y][x] = temp; 
+        } 
+    } 
+
+    shapeHeight = 0;
+    for ( int row = 0; row < selected.size(); row++ ) {
+        bool found = false;
+        for ( int cell = 0; cell < selected[row].size(); cell++ ) {
+            if (selected[row][cell] == 1) {
+                found = true;
+            }
+        }
+        if ( found ) {
+            shapeHeight++;
+        }
+    }
 }
 
 void Shape::draw() {
@@ -173,8 +228,7 @@ void Shape::draw() {
 
         init_pair(2, color, -1);
         attrset(COLOR_PAIR(2));
-
-        for ( int i = 0; i < shapeHeight; i++  ) {
+        for ( int i = 0; i < selected.size(); i++  ) {
             // for each line;
             vector<bool> line = selected[i];
             for ( int i = 0; i < 4; i++ ) {
@@ -182,6 +236,9 @@ void Shape::draw() {
                 if ( line[i] ) {
                     // need to draw two side by side fullblocks;
                     mvprintw(currentPos[0], currentPos[1], string("██").c_str());
+                }
+                else {
+                    mvprintw(currentPos[0], currentPos[1], string("").c_str());
                 }
                 currentPos[1] += 2;
             }
@@ -195,9 +252,23 @@ void Shape::draw() {
 
 void Shape::fall() {
 
+    bool cannotFall = false;
+    vector<int> coords = charCoords(selected, trCoord);
+    cs = ""; 
+    for ( int i = 0; i < coords.size(); i = i + 2) {
+        string check = currentWin[coords[i]][coords[i + 1]];
+        if ( check != " " ) {
+            cannotFall = true;
+        };
+    }
     // psuedocode:
-
-     trCoord[0]++;   // just move down the shape, no need to draw here
+    if (cannotFall) {
+        dead = true;
+    }
+    else {
+        trCoord[0]++;
+    }
+    // just move down the shape, no need to draw here
     // if !( spaceBelowAfterMoveDown ) {
     //  //cannot move down any more, incorporate this one into the board
     //  screen.drawshape(selected, trcoord[0], trcoord[1]);
@@ -206,7 +277,19 @@ void Shape::fall() {
 }
 
 void Shape::move(int movetype) {
+    if ( movetype == 1 ) {
+        // move left
+        trCoord[1] -= 2;
 
+    }
+    else if ( movetype == 2 ) {
+        // move right
+        trCoord[1] += 2;
+    }
+    else if ( movetype == 3) {
+        // move down
+        trCoord[0] += 1;
+    }
 }
 
 void Shape::ground(int framerate) {
